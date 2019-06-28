301fbc6327567331e55664163e8fe866c35aff88
Richard Harris <richard@idestini.com>
2019-06-28 16:01:44 -0500

    Added Scarab UI, began dev

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <img alt="Pokedex" src="../assets/Pokeball.png">
+        <h1>Pokedex</h1>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  img {
+    width: 110px;
+    margin: 1rem;
+    float: left;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './styles/scarab/css/scarab.css'
 
 Vue.config.productionTip = false
 

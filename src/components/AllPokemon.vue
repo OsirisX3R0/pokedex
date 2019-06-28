336683f358e38908a229a74d2c93946ec812c7e2
Richard Harris <richard@idestini.com>
2019-06-28 17:14:32 -0500

    Added list of pokemon

--- src/components/AllPokemon.vue
+++ src/components/AllPokemon.vue
@@ -1,13 +1,35 @@
 <template>
-    
+    <div>
+        <h1>Pokemon</h1>
+        <h3>Count: {{pokemon.count}}</h3>
+
+        <PokemonItem v-for="(pokemon, index) in pokemon.results" :key="index" :pokemon="pokemon" />
+    </div>
 </template>
 
 <script>
-export default {
+import PokemonItem from './PokemonItem'
 
+export default {
+  name: 'AllPokemon',
+  components: {PokemonItem},
+  data() {
+      return {
+          pokemon: null
+      }
+  },
+  methods: {
+
+  },
+  async created() {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000`);
+    this.pokemon = await res.json();
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+    h1 {
+        text-align: center
+    }
 </style>
---
+++ src/components/PokemonItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card">
+      <div class="card-head">
+        <h3>
+          <router-link to="/pokemon" class="dark">{{pokemon.name}}</router-link>
+        </h3>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PokemonItem",
+    props: ["pokemon"],
+    data() {
+        return {
+        }
+    },
+    methods: {
+      
+    }
+}
+</script>
+
+<style scoped>
+    a {
+        text-transform: capitalize;
+    }
+</style>

0f307ab2c947ced2ecb81b15ec1762e66134e973
Richard Harris <richard@idestini.com>
2019-07-05 13:32:12 -0500

    Updated layout for Pokemon Detail

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,86 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 
+    .cap {
+        text-transform: capitalize;
+    }
+
+    .hp {
+        color: #f00;
+    }
+    .atk {
+        color: #f08030;
+    }
+    .def {
+        color: #f8d030;
+    }
+    .satk {
+        color: #6890f0;
+    }
+    .sdef {
+        color: #78c850;
+    }
+    .spd {
+        color: #f85888;
+    }
+
+    .class {
+        font-weight: 500;
+    }
+    .normal {
+        color: #a8a878;
+    }
+    .fire {
+        color: #f08030;
+    }
+    .fighting {
+        color: #c03028;
+    }
+    .water {
+        color: #6890f0;
+    }
+    .flying {
+        color: #a890f0;
+    }
+    .grass {
+        color: #78c850;
+    }
+    .poison {
+        color: #a040a0;
+    }
+    .electric {
+        color: #f8d030;
+    }
+    .ground {
+        color: #e0c068;
+    }
+    .psychic {
+        color: #f85888;
+    }
+    .rock {
+        color: #b8a038;
+    }
+    .ice {
+        color: #98d8d8;
+    }
+    .bug {
+        color: #a8b820;
+    }
+    .dragon {
+        color: #7038f8;
+    }
+    .ghost {
+        color: #705898;
+    }
+    .dark {
+        color: #705848;
+    }
+    .steel {
+        color: #b8b8d0;
+    }
+    .fairy {
+        color: #ee99ac;
+    }
 </style>
--- src/components/PokemonDetail.vue
+++ src/components/PokemonDetail.vue
@@ -4,67 +4,76 @@
             <h3>#{{info.id}} <span class="cap">{{info.species.name}}</span></h3>
         </div>
         <div class="card-body">
-            <img :src="info.sprites.front_default" />
-            <!-- <img :src="info.sprites.back_default" /> -->
-            <span v-for="(type, index) in info.types" :key="type.slot" class="cap type">
-                <span
-                class="class"
-                :class="{
-                    normal:type.type.name=='normal',
-                    fire:type.type.name=='fire',
-                    fighting:type.type.name=='fighting',
-                    water:type.type.name=='water',
-                    flying:type.type.name=='flying',
-                    grass:type.type.name=='grass',
-                    poison:type.type.name=='poison',
-                    electric:type.type.name=='electric',
-                    ground:type.type.name=='ground',
-                    psychic:type.type.name=='psychic',
-                    rock:type.type.name=='rock',
-                    ice:type.type.name=='ice',
-                    bug:type.type.name=='bug',
-                    dragon:type.type.name=='dragon',
-                    ghost:type.type.name=='ghost',
-                    dark: type.type.name=='dark',
-                    steel: type.type.name=='steel',
-                    fairy: type.type.name=='fairy'
-                }">{{type.type.name}}</span>
-                <span v-if="index != info.types.length-1">/ </span>
-            </span>
+            <div class="text-right">
+                <router-link to="/pokemon">&lt; Back</router-link>
+            </div>
+            <div class="top-info">
+                <img :src="info.sprites.front_default" />
+                <!-- <img :src="info.sprites.back_default" /> -->
+                <span class="types">
+                    <span>
+                        <span v-for="(type, index) in types" :key="index" class="cap type">
+                            <span
+                            class="class"
+                            :class="{
+                                normal:type.type.name=='normal',
+                                fire:type.type.name=='fire',
+                                fighting:type.type.name=='fighting',
+                                water:type.type.name=='water',
+                                flying:type.type.name=='flying',
+                                grass:type.type.name=='grass',
+                                poison:type.type.name=='poison',
+                                electric:type.type.name=='electric',
+                                ground:type.type.name=='ground',
+                                psychic:type.type.name=='psychic',
+                                rock:type.type.name=='rock',
+                                ice:type.type.name=='ice',
+                                bug:type.type.name=='bug',
+                                dragon:type.type.name=='dragon',
+                                ghost:type.type.name=='ghost',
+                                dark: type.type.name=='dark',
+                                steel: type.type.name=='steel',
+                                fairy: type.type.name=='fairy'
+                            }">{{type.type.name}}</span>
+                            <span v-if="index != info.types.length-1">/ </span>
+                        </span>
+                    </span>
+                </span>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th colspan="2">Stats</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th class="hp">HP</th>
-                        <td>{{info.stats[5].base_stat}}</td>
-                    </tr>
-                    <tr>
-                        <th class="atk">Attack</th>
-                        <td>{{info.stats[4].base_stat}}</td>
-                    </tr>
-                    <tr>
-                        <th class="def">Defense</th>
-                        <td>{{info.stats[3].base_stat}}</td>
-                    </tr>
-                    <tr>
-                        <th class="satk">Special Attack</th>
-                        <td>{{info.stats[2].base_stat}}</td>
-                    </tr>
-                    <tr>
-                        <th class="sdef">Special Defense</th>
-                        <td>{{info.stats[1].base_stat}}</td>
-                    </tr>
-                    <tr>
-                        <th class="spd">Speed</th>
-                        <td>{{info.stats[0].base_stat}}</td>
-                    </tr>
-                </tbody>
-            </table>
+                <table>
+                    <thead>
+                        <tr>
+                            <th colspan="2">Base Stats</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th class="hp">HP</th>
+                            <td>{{info.stats[5].base_stat}}</td>
+                        </tr>
+                        <tr>
+                            <th class="atk">Attack</th>
+                            <td>{{info.stats[4].base_stat}}</td>
+                        </tr>
+                        <tr>
+                            <th class="def">Defense</th>
+                            <td>{{info.stats[3].base_stat}}</td>
+                        </tr>
+                        <tr>
+                            <th class="satk">Special Attack</th>
+                            <td>{{info.stats[2].base_stat}}</td>
+                        </tr>
+                        <tr>
+                            <th class="sdef">Special Defense</th>
+                            <td>{{info.stats[1].base_stat}}</td>
+                        </tr>
+                        <tr>
+                            <th class="spd">Speed</th>
+                            <td>{{info.stats[0].base_stat}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </template>
@@ -79,92 +88,43 @@ export default {
     },
     methods: {
       
+    },
+    computed: {
+        types: function () {            
+            return this.info.types.slice().sort((a, b) => a.slot - b.slot);
+        }
     }
 }
 </script>
 
 <style scoped>
     img {
+        width: 128px;
         float: left;
     }
-    .cap {
-        text-transform: capitalize;
+    .types {
+        display: grid;
+        align-items: center;
     }
-
-    .hp {
-        color: #f00;
+    table thead th {
+        text-align: center;
     }
-    .atk {
-        color: #f08030;
+    .top-info {
+        display: grid;
+        grid-template-columns: 128px 1fr;
+        grid-template-rows: 128px 1fr;
     }
-    .def {
-        color: #f8d030;
+    .top-info table {
+        grid-column: span 2;
     }
-    .satk {
-        color: #6890f0;
-    }
-    .sdef {
-        color: #78c850;
-    }
-    .spd {
-        color: #f85888;
-    }
-
-    .class {
-        font-weight: 500;
-    }
-    .normal {
-        color: #a8a878;
-    }
-    .fire {
-        color: #f08030;
-    }
-    .fighting {
-        color: #c03028;
-    }
-    .water {
-        color: #6890f0;
-    }
-    .flying {
-        color: #a890f0;
-    }
-    .grass {
-        color: #78c850;
-    }
-    .poison {
-        color: #a040a0;
-    }
-    .electric {
-        color: #f8d030;
-    }
-    .ground {
-        color: #e0c068;
-    }
-    .psychic {
-        color: #f85888;
-    }
-    .rock {
-        color: #b8a038;
-    }
-    .ice {
-        color: #98d8d8;
-    }
-    .bug {
-        color: #a8b820;
-    }
-    .dragon {
-        color: #7038f8;
-    }
-    .ghost {
-        color: #705898;
-    }
-    .dark {
-        color: #705848;
-    }
-    .steel {
-        color: #b8b8d0;
-    }
-    .fairy {
-        color: #ee99ac;
+    @media screen and (min-width: 651px) {
+        .top-info {
+            grid-template-columns: 140px 1fr;
+            grid-template-rows: 128px 1fr;
+        }
+        .top-info table {
+            grid-column: 2 / -1;
+            grid-row: 1 / -1;
+        }
     }
 </style>
